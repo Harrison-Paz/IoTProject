c42c81431969daa61dbe20d524ebb208247c02e7
seew3l <luisrleccar@hotmail.com>
2023-01-11 00:23:43 -0500

    Arreglos

--- projectIoT/panel/urls.py
+++ projectIoT/panel/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sprinkler/', views.view_sprinklers, name='list_sprinklers'),
+    path('', views.view_sprinklers, name='list_sprinklers'),
     path('plan/', views.view_plans, name='list_plans')
 ]
--- projectIoT/projectIoT/urls.py
+++ projectIoT/projectIoT/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from panel import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('panel/', include('panel.urls'))
+    path('', include('panel.urls'))
 ]
